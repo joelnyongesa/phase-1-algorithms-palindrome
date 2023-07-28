c3fa354b55b83c791c44b3f47d0157ee5d177b67
joelnyongesa <joelnyongesa148@gmail.com>
2023-07-28 12:56:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // Create two arrays to store the words, one to store the words and the other to store the words in a reversed manner
+  let wordArray = [...word] 
+  let wordArrayReversed = structuredClone(wordArray).reverse() //Deep copy
+
+  // Join the two arrays, then compare them.
+  if (wordArray.join('') === wordArrayReversed.join('')){
+    // return true
+    return true
+  } else{
+    // return false
+    return false
+  }
 }
+
 
 /* 
   Add your pseudocode here
+  Create an array to store the words 
+  Create another array to store the words, in a reversed manner.
+  Compare the two arrays after joining them to see if they match:
+    if they match:
+      return true
+    else:
+      return false
 */
 
 /*
   Add written explanation of your solution here
+  The isPalindrome() function takes a word as a parameter.
+  Create an array to store the single elements of the word.
+  Create another array of reversed array created in previous step.
+  Loop over the individual elements of the array, if they are equal, return true
+  If the individual elements are not equal, return false.
 */
 
 // You can run `node index.js` to view these console logs
